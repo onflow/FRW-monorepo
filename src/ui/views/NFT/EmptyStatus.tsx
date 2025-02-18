a99f312db8bfc725fe8b8af3cb9cae501835e97c
Tom Haile <bradford.haile@gmail.com>
2025-02-18 12:53:55 -0600

    update nft empty states

--- src/ui/views/NFT/EmptyStatus.tsx
+++ src/ui/views/NFT/EmptyStatus.tsx
@@ -7,7 +7,6 @@ import empty_status from '../../FRWAssets/image/empty_status.svg';
 const useStyles = makeStyles(() => ({
   emptyBox: {
     margin: '48px auto 84px auto',
-    width: '262px',
     height: '249px',
     justifyContent: 'center',
     alignContent: 'center',
@@ -15,6 +14,21 @@ const useStyles = makeStyles(() => ({
   },
   emptyImg: {
     margin: '0 auto auto auto',
+  },
+  title: {
+    fontSize: '16px',
+    lineHeight: '24px',
+    color: '#777E90',
+    fontWeight: 600,
+    marginTop: '16px',
+    marginBottom: '4px',
+    width: '100%',
+  },
+  subtitle: {
+    fontSize: '14px',
+    lineHeight: '20px',
+    color: '#777E90',
+    width: '100%',
   },
 }));
 
@@ -25,10 +39,10 @@ function EmptyStatus() {
     <>
       <Box className={classes.emptyBox}>
         <img className={classes.emptyImg} src={empty_status} height="167px" />
-        <Typography component="div" variant="subtitle1">
+        <Typography className={classes.title}>
           {chrome.i18n.getMessage('We__did__not__find__anything__here')}
         </Typography>
-        <Typography component="div" variant="body2" color="text.nonselect">
+        <Typography className={classes.subtitle}>
           {chrome.i18n.getMessage('Looking__forward__to__your__new__discovery')}
         </Typography>
       </Box>
