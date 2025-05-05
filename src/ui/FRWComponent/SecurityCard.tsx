5b6b4263a0080b1afe3c9f52ec363d78a16bc8b0
zzggo <han0303006@gmail.com>
2025-05-05 12:54:33 +1000

    feat: add security tab on token detail
    Closes #829

---
+++ src/ui/FRWComponent/SecurityCard.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { Box, Typography, Tooltip } from '@mui/material';
+import React from 'react';
+
+import type { CoinItem } from '@/shared/types/coin-types';
+
+export const SecurityCard: React.FC<{ tokenInfo: CoinItem }> = ({ tokenInfo }) => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'neutral.main',
+        borderRadius: '12px',
+        p: 2,
+        mb: 2,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          mb: 1,
+        }}
+      >
+        <Typography>Security</Typography>
+      </Box>
+
+      <Box sx={{ mb: 1 }}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            mb: 0.5,
+          }}
+        >
+          <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+            Verifed
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {tokenInfo.isVerified ? 'Yes' : 'No'}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            mb: 0.5,
+          }}
+        >
+          <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 'bold' }}>
+            Contract Address
+          </Typography>
+          <Tooltip title={tokenInfo.address} arrow placement="top">
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ textDecoration: 'underline', cursor: 'pointer' }}
+              onClick={() => {
+                if (tokenInfo.address) {
+                  navigator.clipboard.writeText(tokenInfo.address);
+                }
+              }}
+            >
+              {tokenInfo.address &&
+                `${tokenInfo.address.slice(0, 5)}...${tokenInfo.address.slice(-5)}`}
+            </Typography>
+          </Tooltip>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default SecurityCard;
