0ae6e257b701cb44360a20db622f96a0e193c5e2
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-05-08 11:36:02 +1000

    Updating dust filter to use USD to determine if dust. A token is dust if the balance is less than 1 USD
    Closes #919

--- src/ui/views/Wallet/Coinlist.tsx
+++ src/ui/views/Wallet/Coinlist.tsx
@@ -261,11 +261,11 @@ const CoinList = ({
           </ListItem>
         ) : !isLoading ? (
           (coins || [])
-            .filter((coin: CoinItem) => {
+            .filter((coin) => {
               if (tokenFilter.hideDust) {
                 const isFlowToken =
                   coin.contractName === 'FlowToken' || coin.unit.toLowerCase() === 'flow';
-                const isAboveDustThreshold = parseFloat(coin.total) >= 1;
+                const isAboveDustThreshold = parseFloat(coin.balanceInUSD) >= 1;
                 if (!isFlowToken && !isAboveDustThreshold) {
                   return false;
                 }
