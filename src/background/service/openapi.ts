9b47983e85014d625d840c1ec889de25e2fc8887
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-06-10 13:13:54 +1000

    Filters out accounts with revoked keys and weight of less than 1000. Validates active accounts when loading
    Closes #1039

--- src/background/service/openapi.ts
+++ src/background/service/openapi.ts
@@ -1858,19 +1858,33 @@ export class OpenApiService {
         ? `https://staging.key-indexer.flow.com/key/${publicKey}`
         : `https://production.key-indexer.flow.com/key/${publicKey}`;
     const result = await fetch(url);
-    const json = await result.json();
+    const json: {
+      publicKey: string;
+      accounts: {
+        address: string;
+        keyId: number;
+        weight: number;
+        sigAlgo: number;
+        hashAlgo: number;
+        isRevoked: boolean;
+        signing: string;
+        hashing: string;
+      }[];
+    } = await result.json();
 
     // Now massage the data to match the type we want
-    const accounts: PublicKeyAccount[] = json.accounts.map((account) => ({
-      address: account.address,
-      publicKey: json.publicKey,
-      keyIndex: account.keyId,
-      weight: account.weight,
-      signAlgo: account.sigAlgo,
-      signAlgoString: account.signing,
-      hashAlgo: account.hashAlgo,
-      hashAlgoString: account.hashing,
-    }));
+    const accounts: PublicKeyAccount[] = json.accounts
+      .filter((account) => !account.isRevoked && account.weight >= 1000)
+      .map((account) => ({
+        address: account.address,
+        publicKey: json.publicKey,
+        keyIndex: account.keyId,
+        weight: account.weight,
+        signAlgo: account.sigAlgo,
+        signAlgoString: account.signing,
+        hashAlgo: account.hashAlgo,
+        hashAlgoString: account.hashing,
+      }));
 
     return accounts;
   };
