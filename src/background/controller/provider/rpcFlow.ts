fd039d8e312488f85d1609ec679fe4b0f9a6be62
zzggo <han0303006@gmail.com>
2025-06-12 15:33:25 +1000

    fixed: skip popup if it ethChainid call
    Closes #1046

--- src/background/controller/provider/rpcFlow.ts
+++ src/background/controller/provider/rpcFlow.ts
@@ -60,6 +60,7 @@ const flowContext = flow
       mapMethod !== 'walletWatchAsset' &&
       mapMethod !== 'walletConnect' &&
       mapMethod !== 'walletDisconnect' &&
+      mapMethod !== 'ethChainId' &&
       !Reflect.getMetadata('SAFE', providerController, mapMethod)
     ) {
       if (!permissionService.hasPermission(origin) || !(await Wallet.isUnlocked())) {
