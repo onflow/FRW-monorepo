3b9070e9efa03933282adcb903b60eb02a845c76
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-07-07 14:20:59 +1000

    Corrected an issue on the mobile import tab
    Closes #1107

--- src/ui/components/import-components/stories/ImportTabs.stories.tsx
+++ src/ui/components/import-components/stories/ImportTabs.stories.tsx
@@ -1,6 +1,8 @@
 import { Box } from '@mui/material';
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
 import React from 'react';
+// eslint-disable-next-line import/order
+import { withRouter } from 'storybook-addon-remix-react-router';
 import { action } from 'storybook/actions';
 import { fn } from 'storybook/test';
 
@@ -13,6 +15,7 @@ const meta = {
   title: 'components/import-components/ImportTabs',
   component: ImportTabs,
   decorators: [
+    withRouter,
     (Story) => {
       useWalletMock.mockReset();
       useWalletMock.mockImplementation(() => ({
