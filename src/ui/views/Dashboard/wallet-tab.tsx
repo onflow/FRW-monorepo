a868e112fc8a06828a1bfcace8bce47e2a1dc6d1
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-07-22 11:16:23 +1000

    Changed to min height
    Closes #1198

--- src/ui/views/Dashboard/wallet-tab.tsx
+++ src/ui/views/Dashboard/wallet-tab.tsx
@@ -63,7 +63,7 @@ const WalletTab = ({ network }) => {
         flexDirection: 'column',
         backgroundColor: 'black',
         width: '100%',
-        height: 'auto',
+        minHeight: '100%',
       }}
     >
       <Tabs
