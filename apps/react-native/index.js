60f250bc778ca7a8b193003dd1569862a753008e
Lea Lobanov <lea.lobanov@icloud.com>
2025-08-08 19:41:27 +0200

    feat: setup revopush in monorepo

--- apps/react-native/index.js
+++ apps/react-native/index.js
@@ -3,7 +3,15 @@
  */
 
 import { AppRegistry } from 'react-native';
+import CodePush from '@revopush/react-native-code-push';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// Wrap App component with CodePush
+const CodePushApp = CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+  updateDialog: true,
+  installMode: CodePush.InstallMode.IMMEDIATE,
+})(App);
+
+AppRegistry.registerComponent(appName, () => CodePushApp);
