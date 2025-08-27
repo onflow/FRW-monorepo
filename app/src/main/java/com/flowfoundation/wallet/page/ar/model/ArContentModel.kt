ba1495448d0dba3861468bc56c66d060043675de
lmcmz <lmcmze@gmail.com>
2025-08-27 16:56:11 +1000

    remove all ar code

--- app/src/main/java/com/flowfoundation/wallet/page/ar/model/ArContentModel.kt
+++
@@ -1,8 +0,0 @@
-package com.flowfoundation.wallet.page.ar.model
-
-class ArContentModel(
-    val onPause: Boolean? = null,
-    val onRestart: Boolean? = null,
-    val onResume: Boolean? = null,
-    val onDestroy: Boolean? = null,
-)
