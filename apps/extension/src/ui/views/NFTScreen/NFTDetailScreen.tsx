93da3092826b652384cc66a5aa0272e0fc736d0f
zzggo <han.zhang@flowfoundation.org>
2025-09-09 16:07:20 +1000

    fix: update nft extension interaction

--- apps/extension/src/ui/views/NFTScreen/NFTDetailScreen.tsx
+++ apps/extension/src/ui/views/NFTScreen/NFTDetailScreen.tsx
@@ -1,23 +1,7 @@
 import { NFTDetailScreen } from '@onflow/frw-screens';
-import { useSendStore } from '@onflow/frw-stores';
-import { useNavigate, useParams } from 'react-router';
 
 const NFTDetailScreenView = (props) => {
-  const navigate = useNavigate();
-  const params = useParams();
-  console.log(params, 'NFTDetailScreenView===');
-  const { id } = params;
-  const { selectedNFTs } = useSendStore();
-  let nft = selectedNFTs.find((nft) => nft.id === id);
-
-  if (!nft) {
-    const nftData = JSON.parse(localStorage.getItem('nftDetailState') || '{}');
-    if (nftData.nft && nftData.nft.id === id) {
-      nft = nftData.nft;
-    }
-  }
-
-  return <NFTDetailScreen nft={nft} selectedNFTs={selectedNFTs} />;
+  return <NFTDetailScreen />;
 };
 
 export default NFTDetailScreenView;
