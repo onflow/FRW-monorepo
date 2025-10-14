0a8b486f700a83ccd73bf055aea8855e3a16f2a5
Lea Lobanov <lea.lobanov@icloud.com>
2025-10-14 21:25:50 +0100

    fix: bubble item

--- app/src/main/java/com/flowfoundation/wallet/page/window/bubble/model/BubbleItem.kt
+++ app/src/main/java/com/flowfoundation/wallet/page/window/bubble/model/BubbleItem.kt
@@ -29,7 +29,7 @@ fun BubbleItem.icon(): Any? {
 fun BubbleItem.title(): String {
     return when (data) {
         is BrowserTab -> data.title().orEmpty()
-        is TransactionState -> transactionTitle()
+        is TransactionState -> data.title()
         else -> ""
     }
 }
@@ -46,6 +46,6 @@ private fun TransactionState.icon(): Any {
     }
 }
 
-private fun transactionTitle(): String {
+private fun TransactionState.title(): String {
     return R.string.pending_transaction.res2String()
 }
