697fb31a4dc19e190081bafd9516357fafcbf14d
Meng <jaymengsniper@gmail.com>
2025-10-31 08:31:44 +0800

    feat: update gas limit

--- app/src/main/java/com/flowfoundation/wallet/manager/flowjvm/CadenceExecutor.kt
+++ app/src/main/java/com/flowfoundation/wallet/manager/flowjvm/CadenceExecutor.kt
@@ -32,7 +32,7 @@ import org.onflow.flow.infrastructure.Cadence.Companion.string
 import java.math.BigDecimal
 
 private const val TAG = "CadenceExecutor"
-const val EVM_GAS_LIMIT = 30000000
+const val EVM_GAS_LIMIT = 16_000_000
 
 suspend fun cadenceQueryAddressByDomainFlowns(domain: String, root: String = "fn"): String? {
     logd(TAG, "cadenceQueryAddressByDomainFlowns(): domain=$domain, root=$root")
