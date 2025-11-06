b625bcb43dc73800a840f5cf09663ac8cdb4631a
Lea Lobanov <lea.lobanov@icloud.com>
2025-11-07 08:46:58 +0900

    fix: fix metro config issues with lottie
    Closes #373

--- packages/ui/src/components/LottieView.tsx
+++ packages/ui/src/components/LottieView.tsx
@@ -1,4 +1,2 @@
-// Web build entry re-exports the web implementation. Metro (RN) will prefer
-// LottieView.native.tsx automatically.
 export { default } from './LottieView.web';
 export type { LottieViewProps, LottieSource } from './LottieView.web';
