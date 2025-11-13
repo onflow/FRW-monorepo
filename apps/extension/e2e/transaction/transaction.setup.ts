433193b75457fdf0aeb885e5cfcdde1e227ea45c
Caos <caosbad@gmail.com>
2025-11-13 20:20:16 +0800

    test: update test scripts
    Closes #382

--- apps/extension/e2e/transaction/transaction.setup.ts
+++ apps/extension/e2e/transaction/transaction.setup.ts
@@ -4,7 +4,7 @@ import { test as setup } from '../utils/loader';
 setup('Import sender and receiver and eoa accounts', async ({ page, extensionId }) => {
   // Lock the extension and import sender and receiver accounts
   setup.slow();
+  await importEoaAccount({ page, extensionId });
   await importSenderAccount({ page, extensionId });
   await importReceiverAccount({ page, extensionId });
-  await importEoaAccount({ page, extensionId });
 });
