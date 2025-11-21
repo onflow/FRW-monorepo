d8a65d4dea147502c305c824627876f8369ef638
lmcmz <lmcmze@gmail.com>
2025-11-22 01:38:42 +1100

    feat: add Sentry logging to platform logger
    Integrated Sentry logger methods
    
    Closes #864

--- apps/extension/src/bridge/PlatformImpl.ts
+++ apps/extension/src/bridge/PlatformImpl.ts
@@ -11,6 +11,7 @@ import {
 import { extractUidFromJwt } from '@onflow/frw-utils';
 import { WalletCoreProvider } from '@onflow/frw-wallet';
 import { addBreadcrumb, type SeverityLevel } from '@sentry/browser';
+import * as Sentry from '@sentry/react';
 import Web3 from 'web3';
 
 // Removed direct service imports - using walletController instead
@@ -584,18 +585,22 @@ class ExtensionPlatformImpl implements PlatformSpec {
     // Console logging for development
     switch (level) {
       case 'debug':
-        console.log(prefix, message, ...args);
+        Sentry.logger.debug(message, ...args);
+        console.debug(prefix, message, ...args);
         break;
       case 'info':
         // Only log info to console in debug mode to keep prod console clean
         if (this.debugMode) {
           console.info(prefix, message, ...args);
         }
+        Sentry.logger.info(message, ...args);
         break;
       case 'warn':
+        Sentry.logger.warn(message, ...args);
         console.warn(prefix, message, ...args);
         break;
       case 'error':
+        Sentry.logger.error(message, ...args);
         console.error(prefix, message, ...args);
         break;
     }
