7ff633166cd8707b517d647adef58b5a1218bcc9
Meng <jaymengsniper@gmail.com>
2025-12-10 08:47:12 +0800

    fix: evm label

--- app/src/main/java/com/flowfoundation/wallet/instabug/InstabugUtils.kt
+++ app/src/main/java/com/flowfoundation/wallet/instabug/InstabugUtils.kt
@@ -68,6 +68,7 @@ fun instabugInitialize(application: Application) {
                 childAccounts.toString()
             )
         report.setUserAttribute("COA", EVMWalletManager.getEVMAddress().orEmpty())
+        report.setUserAttribute("EOA", WalletManager.getEOAAddress().orEmpty())
         report.setUserAttribute("Network", chainNetWorkString())
     }
     val configuration = ProactiveReportingConfigs.Builder()
