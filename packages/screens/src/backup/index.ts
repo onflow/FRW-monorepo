ded3b40accdbb2ab1744c27bc1f3fbd71a2eab01
Lea Lobanov <lea.lobanov@icloud.com>
2025-12-20 00:12:19 +0900

    chore: clean up
    Closes #1062

--- packages/screens/src/backup/index.ts
+++ packages/screens/src/backup/index.ts
@@ -3,5 +3,3 @@ export type { BackupTipScreenProps } from './BackupTipScreen.query';
 
 export { BackupMnemonicScreen } from './BackupMnemonicScreen.query';
 export type { BackupMnemonicScreenProps } from './BackupMnemonicScreen.query';
-
-export * from './backup.types';
