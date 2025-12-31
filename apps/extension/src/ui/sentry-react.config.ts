c2168e731b6d3a59ef8d7d59f3e5d4435cee8873
Caos <caosbad@gmail.com>
2025-12-31 09:24:46 +0800

    chore: add senrty ip config
    Closes #1123

--- apps/extension/src/ui/sentry-react.config.ts
+++ apps/extension/src/ui/sentry-react.config.ts
@@ -14,6 +14,7 @@ import { SENTRY_BASIC_CONFIG, SENTRY_NETWORK_ALLOW_URLS } from '../shared/consta
 init({
   ...SENTRY_BASIC_CONFIG,
   enableLogs: true,
+  sendDefaultPii: true,
   integrations: [
     browserTracingIntegration(),
     browserProfilingIntegration(),
