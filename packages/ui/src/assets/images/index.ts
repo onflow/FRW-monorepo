0fda7101a368d035bcae19d8f7be84cc0f91f186
Lea Lobanov <44328396+lealobanov@users.noreply.github.com>
2026-01-05 16:53:59 +0900

    1063 blocto missing components from dev (#1139)
    * fix: add missing components from dev
    
    Closes #1063
    
    * fix: add missing components from dev
    
    Closes #1063
    
    * fix: add missing components from dev
    
    Closes #1063
    
    * fix: add missing components from dev
    
    Closes #1063
    
    * fix: add missing components from dev
    
    Closes #1063
    
    * fix: add missing components from dev
    
    Closes #1063

---
+++ packages/ui/src/assets/images/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Image assets for the UI package
+ * These are exported so they can be used by consuming packages
+ */
+
+// Onboarding images
+export const onboardingImages = {
+  fullBackgroundLight: require('./onboarding/full_bg_light.png'),
+};
+
+// Re-export for easier access
+export const { fullBackgroundLight } = onboardingImages;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -85,7 +85,13 @@ export * from './components/Toast';
 export * from './components/TokenCard';
 export * from './components/TokenSelectorModal';
 export * from './components/TransactionFeeSection';
+export * from './components/WarningCard';
 export * from './foundation/SegmentedControl';
+
+// Export onboarding components
+export * from './components/onboarding/TipCard';
+export * from './components/onboarding/MnemonicGrid';
+export { ShieldAnimation } from './components/ShieldAnimation';
 
 export * from './components/TokenAmountInput';
 
@@ -103,6 +109,7 @@ export { extensionTamaguiConfig } from './theme/extension';
 
 // Export layout components
 export * from './layout/BackgroundWrapper';
+export * from './layout/OnboardingBackground';
 
 // Export types
 export * from './types';
