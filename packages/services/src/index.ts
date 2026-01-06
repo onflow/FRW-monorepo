97460294ddb80f149ee776fe59d3d01f3b4db02b
cat <zhouxl72@126.com>
2026-01-06 16:24:46 +0800

    fix: build error
    Closes #1063

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -5,7 +5,6 @@ export { KeyRotationService } from './KeyRotationService';
 export { NFTService } from './NFTService';
 export { RecentRecipientsService } from './RecentRecipientsService';
 export { TokenService } from './TokenService';
-export { KeyRotationService } from './KeyRotationService';
 
 // Convenience functions for accessing services through context
 export {
