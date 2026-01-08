7f7898451d694f03277aba0f11e208f7134bbeb1
cat <zhouxl72@126.com>
2026-01-08 15:53:30 +0800

    feat: add screen case
    Closes #1063

--- packages/types/src/Bridge.ts
+++ packages/types/src/Bridge.ts
@@ -91,7 +91,7 @@ export interface SendToConfig {
  * Initial props for the app
  */
 export interface InitialProps {
-  screen: 'send-asset' | 'token-detail';
+  screen: 'send-asset' | 'backup-tip';
   sendToConfig?: string;
 }
 
