0abf6b8fefcd19935e155bbde34d2d61eb276b38
lmcmz <lmcmze@gmail.com>
2026-01-12 16:36:46 +1100

    feat: wait txid to be sealed
    Closes #1063

--- packages/cadence/src/index.ts
+++ packages/cadence/src/index.ts
@@ -70,3 +70,4 @@ export function createCadenceService(bridge: CadenceBridge): CadenceService {
 
 // Re-export types and services from generated file
 export { CadenceService, addresses, type Result } from './cadence.generated';
+export * as fcl from '@onflow/fcl';
