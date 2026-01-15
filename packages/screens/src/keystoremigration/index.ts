e4d7e651aaa4d21b5d089de318e90d8faafa1bc5
Lea Lobanov <44328396+lealobanov@users.noreply.github.com>
2026-01-15 10:13:16 +0900

    feat: new screen for keystore migration (#1166)

---
+++ packages/screens/src/keystoremigration/index.ts
@@ -0,0 +1,4 @@
+export { KeystoreMigrationTipScreen } from './KeystoreMigrationTipScreen.query';
+export type { KeystoreMigrationTipScreenProps } from './KeystoreMigrationTipScreen.query';
+
+export * from './keystoremigration.types';
